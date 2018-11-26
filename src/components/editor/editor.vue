112b9364b459a01f9d3ca325ce06f09c967fe833
yanghao <1091174771@163.com>
2018-11-27 00:26:55 +0800

    tinymce

--- src/components/editor/editor.vue
+++ src/components/editor/editor.vue
@@ -5,17 +5,20 @@
 </template>
 
 <script type="text/ecmascript-6">
-import tinymce from 'tinymce/tinymce'
-import 'tinymce/themes/modern/theme'
-import 'tinymce/plugins/image'
-import 'tinymce/plugins/link'
-import 'tinymce/plugins/code'
-import 'tinymce/plugins/table'
-import 'tinymce/plugins/textcolor'
-import 'tinymce/plugins/paste'
-import 'tinymce/plugins/colorpicker'
-const INIT = 0
-const CHANGED = 2
+import tinymce from 'tinymce/tinymce';
+import 'tinymce/themes/modern/theme';
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/textcolor';
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/colorpicker';
+import 'tinymce/plugins/fullscreen';
+const INIT = 0;
+const CHANGED = 2;
+// var EDITOR = null;
 export default {
   data() {
     return {
@@ -28,8 +31,9 @@ export default {
   },
   props:{
     value:{
+        default:'',
         type:String,
-        editor:null,
+        // editor:null,
         required:true
     },
     setting:{},
@@ -51,25 +55,79 @@ export default {
     }
   },
   watch:{
-      value(val){
-          console.log(val)
-          if(this.status === INIT || tinymce.activeEditor.getContent() !== val){
-              tinymce.activeEditor.setContent(val)
-          }
-          this.CHANGED = CHANGED
-      }
+    //   value(val){
+    //     console.log('init ' + val)
+    //     if(this.status === INIT || tinymce.activeEditor.getContent() !== val){
+    //         tinymce.activeEditor.setContent(val)
+    //     }
+    //     this.status = CHANGED
+    //   }
   },
   computed:{},
-  methods:{},
+  methods:{
+    handleImgUpload(blobInfo,success,failure){
+        let formdata = new FormData()
+        if(blobInfo.blob().size > this.maxSize){
+            failure('文件体积过大')
+        }
+        if(this.accept.indexOf(blobInfo.blob().type) >= 0){
+            this.uploadPic()
+        }else{
+            failure('图片格式错误')
+        }
+    },
+    uploadPic(){
+
+    }
+  },
   created(){},
   mounted(){
-      let _this=this;
-      let setting ={
+      const _this=this;
+      const setting ={
         selector:'#'+_this.id,
+        menubar: true,//去除文件栏
+        branding: false,//去除右下角的'由tinymce驱动'
         upload_image_url:'',//配置的上传图片的路由
-        language:''
+        language:'zh_CN', //语言设置为中文
+        language_url:'../../../static/tinymce/zh_CN.js', //导入中文语言文件
+        skin_url:'../../../static/tinymce/skins/lightgray',
+        init_instance_callback:function(editor){
+            // EDITOR = editor;
+            editor.on('input change undo redo', () =>{
+                _this.$emit('input',editor.getContent())
+            })
+        },
+        inser_button_items:'image link | inserttable',  //指定哪些按钮在插入菜单显示
+        // CONFIG: Paste
+        paste_retain_style_properties:'all',  //此选项允许您指定在粘贴MS Word中的内容时要保留的样式，"all"表示要保留所有样式
+        paste_word_valid_elements: '*[*]', // word需要它
+        paste_data_images: true, // 粘贴的同时能把内容里的图片自动上传，非常强力的功能
+        paste_convert_word_fake_lists: false, // 插入word文档需要该属性
+        paste_webkit_styles: 'all',
+        paste_merge_formats: true,
+        nonbreaking_force_tab: false,
+        paste_auto_cleanup_on_paste: false,
+        // CONFIG: Font
+        // fontsize_formats: '10px 11px 12px 14px 16px 18px 20px 24px 26px 30px',
+        // 'plugins': [
+        //   'advlist link image',
+        //   'code',
+        //   'table paste textcolor colorpicker'
+        // ], // 配置
+        // 'toolbar_items_size': 'small',
+        // 'block_formats': 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6;',
+        // 'toolbar1': 'table |insertfile undo redo | formatselect | link unlink | uploadimg image media', // 工具栏1
+        // 'toolbar2': ' fontsizeselect | forecolor backcolor | fontselect bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent | removeformat', // 工具栏2
+        // // 图片上传
+        // images_upload_handler: function(blobInfo, success, failure) {
+        //     this.handleImgUpload(blobInfo,success,failure)
+        // },
       }
-
+      Object.assign(setting, _this.setting)
+      tinymce.init(setting)
+  },
+  beforeDestroy:function () {
+    tinymce.get(this.id).destroy()
   }
 }
 </script>
--- src/view/knowledge_notice/power_knowledge/power_knowledge.vue
+++ src/view/knowledge_notice/power_knowledge/power_knowledge.vue
@@ -11,14 +11,23 @@
           </Row>
         </Card>
       </i-col>
+      <i-col :span="24">
+        <editor class="power_knowledge_editor" 
+          :value="content"
+          :setting="editorSetting"
+          @input="editors"
+          :url="Url"
+          :max-size="MaxSize"
+          :accept="Accept"
+          :with-credentials="withCredentials"
+          @on-upload-fail="onEditorReady"
+          @on-upload-success="onEditorUploadComplete"></editor>
+      </i-col>
       <i-col :span="24" style="margin-top:15px;text-align: right;padding-right:20px;">
-        <Button type="primary" >保存</Button>
+        <Button type="primary" @click="editors">保存</Button>
       </i-col>
     </Row>
-    <!-- <i-editor v-model="content" :write-name="title">
-
-    </i-editor>
-    {{content}} -->
+    
   </div>
 </template>
 
@@ -27,8 +36,15 @@ import Editor from '@comp/editor' //引入富文本编辑器
 export default {
   data() {
     return {
-      content:'',
       knowledge_titile:'',
+      editorSetting:{ // 配置富文本编辑器高
+        height: 300
+      },
+      Url:'',
+      MaxSize: 75765, // 文件大小
+      Accept: 'image/png,image/jpg,image/jpeg', // 图片文件格式
+      withCredentials: true,
+      content: '' // 富文本编辑器双向绑定的内容
     }
   },
   components: {
@@ -38,7 +54,21 @@ export default {
   watch:{},
   computed:{},
   methods:{
-    
+    editors(item){ // editor组件传过来的值赋给content
+      console.log(item)
+      // this.content = val
+    },
+    onEditorReady(ins, ina) { // 上传失败的函数
+        console.log('ins')
+        console.log(ins)
+        console.log(ina)
+    },
+    onEditorUploadComplete(json) { // 处理上传图片后返回数据，添加img标签到编辑框内
+        console.log('json')
+        console.log(json)
+        console.log(json[0].data.filePath)
+        this.content = this.content + '<img src=' + json[0].data.filePath + '>'
+    }
   },
   created(){},
   mounted(){}
